c2453f6a9603552111bbd413de45c06b54b17c4c
MeridaNoe <105667207+MeridaNoe@users.noreply.github.com>
2022-11-11 08:33:32 -0600

    Initial commit
    primer commit

---
+++ personalApp4A/src/app/heroes/heores.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { HeroeMainPageComponent } from './main-page/main-page.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+declarations:[HeroeMainPageComponent, ListComponent], //Todos los componentes del modulo
+imports:[],// Solo se agrean otros modulos
+providers:[], //Se agregan los servicios
+exports: [HeroeMainPageComponent]
+})
+export class HeroesModule{}
---
+++ personalApp4A/src/app/heroes/main-page/main-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'heroe-main-page',
+  templateUrl:'./main-page.component.html'
+})
+export class HeroeMainPageComponent{
+
+}
